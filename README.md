53735369425616b3c5dd5e7dd24ac89b209d4582
Robin Cher <robincher@gmail.com>
2018-09-16 19:11:26 +0800

    Add class file and ReadMe

---
+++ README.md
@@ -0,0 +1,25 @@
+# GO API Demo
+
+Demo REST API built with Golang
+
+Before you start , make sure you have installed and configure your Go binary correctly.
+
+## Tech Stack
+* POSTMAN or Restlet for Testing
+* Go 1.11
+* Go Libraries
+    - MUX
+
+## Walkthrough
+
+### Starting the REST API Server
+```
+go build -o ./bin/output.exe .
+
+./bin/output.exe (or double click on the exe file)
+```
+
+Execute your HTTP request @ http://localhost:8000 
+
+## Reference
++ [Codementor Tutorial](https://www.codementor.io/codehakase/building-a-restful-api-with-golang-a6yivzqdo)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+func getPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+
+func createPerson(w http.ResponseWriter, r *http.Request) {
+	var person Person
+	params := mux.Vars(r)
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+
+}
+func deletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
+}
+
+// Main function
+func main() {
+	router := mux.NewRouter()
+
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Cena", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Momo", Address: &Address{City: "City Z", State: "State Y"}})
+
+	//Handle Routes
+	router.HandleFunc("/people", getPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", getPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", createPerson).Methods("POST")
+	router.HandleFunc("/people/{id}", deletePerson).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
